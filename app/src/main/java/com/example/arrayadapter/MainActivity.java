c016c2d6513d5fd30a4494f981be7ae1229dd449
MoringaSchool <you@example.com>
2020-01-30 14:15:40 +0300

    initial commit

---
+++ app/src/main/java/com/example/arrayadapter/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.arrayadapter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.ListView);
+
+        final String[] strArr = new String[]{
+                "Samora",
+                "Mark",
+                "Mburia",
+                "Shady",
+                "Hmmerton",
+        };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, strArr);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                String value = strArr[i];
+            }
+        });
+        {
+        }
+    }
+}
